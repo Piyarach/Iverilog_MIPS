81f0ed306667696383e06a424969547d6e839370
Piyarach <piyaracht59@email.nu.ac.th>
2016-11-24 12:31:17 +0700

    Add files via upload

---
+++ test-bench/adder_32_tb.v
@@ -0,0 +1,20 @@
+`include "../src/adder_32_bit.v"
+module adder_32_bit_tb;
+	
+	reg signed [31:0] a , b;
+	reg cin;
+	wire signed [31:0] sum;
+	wire cout;
+	
+	adder_32_bit a1(a,b,cin,sum,cout);
+	initial begin
+		$monitor(a, ,b, , ,sum, ,cout);
+		a = 0; b = 0; cin = 0;
+		#1 a = 5; b = 9; cin = 1'b0;
+		#1 a = 32'b00000000000000000000000000010000; b = 32'b00000000000000000000000000100000; cin = 1'b0;
+		#1 a = 128; b = 64; cin = 1'b0;
+		#1 a = -64; b = 64; cin = 1'b0;
+		#1 a = -64; b = 96; cin = 1'b0;
+		#1 $finish;
+	end
+endmodule
---
+++ test-bench/subtract_32_bit_tb.v
@@ -0,0 +1,21 @@
+`include "../src/adder_32_bit.v"
+`include "../src/subtract_32_bit.v"
+module subtract_32_bit_tb;
+	
+	reg signed [31:0] a , b;
+	reg cin;
+	wire signed [31:0] sum;
+	wire cout;
+	
+	subtract_32_bit s1(a,b,sum,cout);
+	initial begin
+		$monitor(a, ,b, , ,sum, ,cout);
+		a = 0; b = 0;
+		#1 a = 7; b = 6;
+		#1 a = -7; b = 6;
+		#1 a = -7; b = -6;
+		#1 a = 6; b = 7;
+		#1 a = 32'b00000000000000000000000000100000; b = 32'b00000000000000000000000000010000;
+		#1 $finish;
+	end
+endmodule
